16dc44ef7c776ad3ed2a2364292f554b85ad04e2
victorhoppenot <victorhoppenot@gmail.com>
2020-07-01 18:20:55 -0400

    added hover highlight

--- game.js
+++ game.js
@@ -1,13 +1,17 @@
 var board = [
     ['__','__','__','__','__','__','__','bQ'],
     ['__','__','__','wB','bB','__','__','__'],
-    ['__','__','bH','__','__','wH','__','__'],
-    ['__','__','__','bK','wR','__','__','__'],
-    ['__','__','__','bR','wK','__','__','__'],
     ['__','__','wH','__','__','bH','__','__'],
-    ['__','__','__','bB','wB','__','__','__'],
+    ['__','__','__','wK','bR','__','__','__'],
+    ['__','__','__','wR','bK','__','__','__'],
+    ['__','__','wH','__','__','bH','__','__'],
+    ['__','__','__','wB','bB','__','__','__'],
     ['wQ','__','__','__','__','__','__','__']
 ]
+var highlights = [];
+
+var hoverX = 0;
+var hoverY = 0;
 
 var assets = ['board','bB','bH','bK','bP','bQ','bR','wB','wH','wK','wP','wQ','wR'];
 var imagesToBeLoaded = assets.length;
@@ -18,8 +22,22 @@ window.onload = function() {
 
     width = c.width;
     height = c.height;
+    colDivider = width/8;
+    rowDivider = height/8;
 
     images = loadImages();
+
+    c.addEventListener('mousemove', onMouseMove);
+};
+
+function onMouseMove(evt){
+    let mousepos = getMousePos(evt);
+    
+    hoverX = bound(mousepos.x, 0, width);
+    hoverY = bound(mousepos.y, 0, height);
+    hoverX = Math.floor(hoverX/colDivider)*colDivider;
+    hoverY = Math.floor(hoverY/rowDivider)*rowDivider;
+    displayBoard();
 }
 
 function loadImages(){
@@ -40,9 +58,17 @@ function loadImages(){
 }
 
 function onAllImagesLoaded(){
+    displayBoard();
+}
+
+function displayBoard(){
     ctx.drawImage(images['board'],0,0,width,height);
-    let colDivider = width/8;
-    let rowDivider = height/8;
+    
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = '#03fc2c';
+    ctx.fillRect(hoverX,hoverY,colDivider,rowDivider);
+    ctx.globalAlpha = 1;
+    
     let rowN = 0;
     for(const row of board){
         let colN = 0;
@@ -58,6 +84,14 @@ function onAllImagesLoaded(){
     }
 }
 
-function displayBoard(){
-    
+function getMousePos(evt){
+    let rect = c.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top,
+    }
 }
+function bound(v, a, b){
+    return Math.max(Math.min(v, b), a);
+}
+
